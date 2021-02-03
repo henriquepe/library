3be60e41b46a7bfab6b9c6f4b18b22a1b08e7145
henriquepe <henrique.p.espindola@gmail.com>
2021-02-02 22:57:10 -0300

    library app

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 require('./database')
 
+const cors = require('cors');
+
 const express = require('express');
 const routes = require('./routes');
 
 const app = express()
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.listen(3333, () => { console.log('server started') })
